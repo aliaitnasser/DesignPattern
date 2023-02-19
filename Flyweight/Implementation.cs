6f26569fcdcbca8ee3175e930b0ec029711b93bb
Ali Ait Nasser <aliii.nassser@gmail.com>
2023-02-19 18:11:12 +0100

    Adding Flyweight Pattern

---
+++ Flyweight/Implementation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+	/// <summary>
+	/// Flyweight
+	/// </summary>
+	public interface ICharacter
+	{
+		void Draw(string fontFamily, int fontSize);
+	}
+
+
+	/// <summary>
+	/// Concrete Flyweight
+	/// </summary>
+	public class CharacterA: ICharacter
+	{
+		private char _actualCharacter = 'a';
+		private string _fontFamily = string.Empty;
+		private int _fontSize;
+
+		public void Draw(string fontFamily, int fontSize)
+		{
+			_fontSize = fontSize;
+			_fontFamily = fontFamily;
+			Console.WriteLine($"Drawing {_actualCharacter}, {_fontFamily}, {_fontSize}");
+		}
+	}
+
+	/// <summary>
+	/// Concrete Flyweight
+	/// </summary>
+	public class CharacterB: ICharacter
+	{
+		private char _actualCharacter = 'b';
+		private string _fontFamily = string.Empty;
+		private int _fontSize;
+
+		public void Draw(string fontFamily, int fontSize)
+		{
+			_fontSize = fontSize;
+			_fontFamily = fontFamily;
+			Console.WriteLine($"Drawing {_actualCharacter}, {_fontFamily}, {_fontSize}");
+		}
+	}
+
+	public class FlyweithFactory
+	{
+		private readonly Dictionary<char, ICharacter> _characters = new ();
+
+		public ICharacter? GetCharacter(char key)
+		{
+			// Does the dictionary contain the one we need
+			if(_characters.ContainsKey(key))
+			{
+				Console.WriteLine("Character reuse.");
+				return _characters[key];
+			}
+			// The Character is not in the dictionary
+			// Create it, store it, return it.
+			Console.WriteLine("Character creation.");
+			switch (key)
+			{
+				case 'a':
+					_characters.Add(key, new CharacterA());
+					return _characters[key];
+				case 'b':
+					_characters.Add(key, new CharacterB());
+					return _characters[key];
+			};
+			return null;
+		}
+	}
+}
---
+++ Flyweight/Program.cs
@@ -0,0 +1,21 @@
+﻿using Flyweight;
+
+Console.Title = "Flyweight";
+
+string text = "abba";
+
+var characterFactory = new FlyweithFactory();
+
+//get all flyweight
+var characters = characterFactory.GetCharacter(text[0]);
+characters?.Draw("Arial", 12);
+
+characters = characterFactory.GetCharacter(text[1]); 
+characters?.Draw("Booksman oldstyle", 18);
+
+characters = characterFactory.GetCharacter(text[2]);
+characters?.Draw("Times new Roman", 12);
+
+characters = characterFactory.GetCharacter(text[3]);
+characters?.Draw("Conic Sans", 10);
+

21945ee8ad1fc33c6b54a348b5f5ce8af0fcc14d
Ali Ait Nasser <aliii.nassser@gmail.com>
2023-02-21 13:38:11 +0100

    Adding the Memento Pattern

---
+++ Memento/Implementation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+	public class LedTV
+	{
+		public string Size { get; set; }
+		public string Prise { get; set; }
+		public bool isFHD { get; set; }
+
+		public LedTV(string size, string prise, bool isFhd)
+		{
+			Size = size;
+			Prise = prise;
+			isFHD = isFhd;
+		}
+
+		public string GetDetails()
+		{
+			return $"Led TV [Size = {Size}, Prise : {Prise}, FHD : {isFHD}]";
+		}
+	}
+
+	/// <summary>
+	/// Memento
+	/// </summary>
+	public class Memento
+	{
+		public LedTV LedTV { get; set;}
+
+		public Memento(LedTV ledTV)
+		{
+			LedTV = ledTV;
+		}
+
+		public string GetDetails()
+		{
+			return $"Memento [Led TV = {LedTV.GetDetails()}]";
+		}
+	}
+
+	/// <summary>
+	/// Caretaker
+	/// </summary>
+	public class CareTaker
+	{
+		private List<Memento> ledTvList = new ();
+		public void AddMemento(Memento memento)
+		{
+			ledTvList.Add(memento);
+			Console.WriteLine($"Led TV snapshot Maintaned by Caretaker: {memento.GetDetails()}");
+		}
+
+		public Memento GetMemento(int index)
+		{
+			return ledTvList[index];
+		}
+	}
+
+	/// <summary>
+	/// Originator
+	/// </summary>
+	public class Originator
+	{
+		public LedTV ledTV;
+
+		public Memento CreateMemento()
+		{
+			return new Memento(ledTV);
+		}
+
+		public void SetMemento(Memento memento)
+		{
+			ledTV = memento.LedTV;
+		}
+		public string GetDetails()
+		{
+			return $"Led TV Generator [Led TV = {ledTV.GetDetails()}]";
+		}
+	}
+
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,23 @@
+﻿using Memento;
+
+Console.Title = "Memento";
+
+Originator Originator = new();
+CareTaker Caretaker = new ();
+
+Originator.ledTV = new LedTV("32", "10000", true);
+Caretaker.AddMemento(Originator.CreateMemento());
+
+Originator.ledTV = new LedTV("40", "20000", true);
+Caretaker.AddMemento(Originator.CreateMemento());
+
+Originator.ledTV = new LedTV("50", "30000", true);
+
+
+Console.WriteLine($"\nOriginator current stats : {Originator.GetDetails()} ");
+
+Originator.ledTV = Caretaker.GetMemento(0).LedTV;
+
+Console.WriteLine($"\nOriginator current stats : {Originator.GetDetails()} ");
+
+

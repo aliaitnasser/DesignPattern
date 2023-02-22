4c4f9855b61905bfd13c6327e99d358486dbae43
Ali Ait Nasser <aliii.nassser@gmail.com>
2023-02-22 11:32:17 +0100

    Adding Mediator Pattern

---
+++ Mediator/Implementation.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+	/// <summary>
+	/// Mediator
+	/// </summary>
+	public interface IChatRoom
+	{
+		void Register(TeamMember teamMember);
+		void Send(string from, string message);
+		void Send(string from, string to, string message);
+	}
+
+	/// <summary>
+	/// Colleage
+	/// </summary>
+	public abstract class TeamMember
+	{
+		private IChatRoom? _chatRoom;
+		public string Name { get; set; }
+
+		public TeamMember(string name)
+		{
+			Name = name;
+		}
+
+		internal void SetChatRoom(IChatRoom chatRoom)
+		{
+			_chatRoom = chatRoom;
+		}
+
+		public void Send(string message)
+		{
+			_chatRoom?.Send(Name, message);
+		}
+		public void Send(string to, string message)
+		{
+			_chatRoom?.Send(Name, to, message);
+		}
+
+		public virtual void Receive(string from, string message)
+		{
+			Console.WriteLine($"From {from}:\n \t{message}\n");
+		}
+	}
+
+	/// <summary>
+	/// Concrete colleage
+	/// </summary>
+	public class Lawyer: TeamMember
+	{
+		public Lawyer(string name) : base(name) { }
+
+		public override void Receive(string from, string message)
+		{
+			Console.WriteLine($"{nameof(Lawyer)} {Name} received :");
+			base.Receive(from, message);
+		}
+	}
+
+	/// <summary>
+	/// Concrete colleage
+	/// </summary>
+	public class Manager: TeamMember
+	{
+		public Manager(string name) : base(name) { }
+
+		public override void Receive(string from, string message)
+		{
+			Console.WriteLine($"{nameof(Manager)} {Name} received :");
+			base.Receive(from, message);
+		}
+	}
+
+	/// <summary>
+	/// Concrete Mediator
+	/// </summary>
+	public class TeamChatRoom: IChatRoom
+	{
+		private readonly Dictionary<string, TeamMember> teamMembers = new(); 
+		public void Register(TeamMember teamMember)
+		{
+			teamMember.SetChatRoom(this);
+			if(!teamMembers.ContainsKey(teamMember.Name)) teamMembers.Add(teamMember.Name, teamMember);
+		}
+
+		public void Send(string from, string message)
+		{
+			foreach(var teamMember in teamMembers.Values)
+			{
+				if(teamMember.Name != from) teamMember.Receive(from, message);
+			}
+		}
+		public void Send(string from, string to, string message)
+		{
+			var teamMember = teamMembers[to];
+			teamMember.Receive(from, message);
+		}
+	}
+
+
+}
---
+++ Mediator/Program.cs
@@ -0,0 +1,18 @@
+﻿using Mediator;
+
+Console.Title = "Mediator";
+
+TeamChatRoom ChatRoom = new ();
+
+var said = new Lawyer("said");
+var issam = new Lawyer("issam");
+var farid = new Manager("farid");
+var mariam = new Manager("mariam");
+
+ChatRoom.Register(said);
+ChatRoom.Register(farid);
+ChatRoom.Register(mariam);
+ChatRoom.Register(issam);
+
+said.Send("issam","Hi, Please find attached the download file");
+mariam.Send("Meeting at 9AM");

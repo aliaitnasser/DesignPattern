e615fd3559d4ce395438a83516803906c9490c6b
Ali Ait Nasser <aliii.nassser@gmail.com>
2023-02-28 12:29:19 +0100

    Adding Iterator Pattern

---
+++ Iterator/Implementation.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public string Country { get; set; }
+
+		public Person(string name, string country)
+		{
+			Name = name;
+			Country = country;
+		}
+	}
+
+	/// <summary>
+	/// Iterator
+	/// </summary>
+	public interface IPeopleIterator
+	{
+		Person First();
+		Person Next();
+		bool IsDone { get; }
+		Person Current { get; }
+	}
+
+	/// <summary>
+	/// Aggregate
+	/// </summary>
+	public interface IPeopleCollection
+	{
+		IPeopleIterator CreateIterator();
+	}
+
+	/// <summary>
+	/// Concrete aggragate
+	/// </summary>
+	public class PeopleCollection: List<Person>, IPeopleCollection
+	{
+		public IPeopleIterator CreateIterator()
+		{
+			return new PeopeIteratore(this);
+		}
+	}
+
+	/// <summary>
+	/// Concrete iterator
+	/// </summary>
+	public class PeopeIteratore: IPeopleIterator
+	{
+		private PeopleCollection _people;
+		private int _current = 0;
+
+		public PeopeIteratore(PeopleCollection people)
+		{
+			_people = people;
+		}
+		public bool IsDone
+		{
+			get { return _current >= _people.Count; }
+		}
+
+		public Person Current
+		{
+			get { return _people.OrderBy(p => p.Name).ToList()[_current]; }
+		}
+
+		public Person First()
+		{
+			_current = 0;
+			return _people.OrderBy(p => p.Name).First();
+		}
+
+		public Person Next()
+		{
+			_current++;
+			if(!IsDone)
+			{
+				return _people.OrderBy(p => p.Name).ToList()[_current];
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,18 @@
+﻿using Iterator;
+
+Console.Title = "Iterator Pattern";
+
+var people = new PeopleCollection();
+
+people.Add(new Person("John", "USA"));
+people.Add(new Person("Bob", "MA"));
+people.Add(new Person("Mary", "KSA"));
+people.Add(new Person("Anna", "UK"));
+
+var iterator = people.CreateIterator();
+
+Console.WriteLine("Iterating over collection:");
+for (var person = iterator.First(); !iterator.IsDone; person = iterator.Next())
+{
+	Console.WriteLine(person.Name);
+}
